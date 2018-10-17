57e650ed97d691640cf831b9d85502cc1db66aa5
Louise Badarani <Louise@Louises-MacBook-Pro.local>
2018-10-17 16:16:16 +0200

    MelaNet debugged

--- MelaNet.py
+++ MelaNet.py
@@ -7,6 +7,10 @@ import os
 
 class MelaData(Dataset):
     def __init__(self, data_dir, label_csv, transforms=None):
+        """
+        data_dir : path to images folder
+        label_csv : path to HAM10000_metadata.csv
+        """
         files = os.listdir(data_dir)
         self.len = len(files)
         
@@ -18,11 +22,12 @@ class MelaData(Dataset):
             images = np.concatenate((images, im), axis = 0)
         self.images = images
         
-        
+        dx_to_num = {'nv' : 0, 'mel': 1, 'bkl': 2, 'df': 3, 'akiec': 4, 'bcc': 5, 'vasc' : 6}        
         labels = pd.read_csv(label_csv)
         labels['image_id_num'] = labels['image_id'].apply(lambda x: int(x.strip('ISIC_')))
-        labels = labels.sort_values(by = 'image_id_num')
+        labels = labels.sort_values(by = 'image_id_num')        
         labels.reset_index(inplace = True, drop = True)
+        labels['label'] = labels['dx'].apply(lambda x: dx_to_num[x])
         
         self.labels = labels
         
@@ -31,10 +36,10 @@ class MelaData(Dataset):
     def __getitem__(self, index):
         img = self.images[index, :, :, :]
         img_t = torch.from_numpy(img) 
-        label = self.labels.loc[index].dx
+        label = self.labels.loc[index].label
         label = np.array(label)
         label_t = torch.from_numpy(label)
         return (img_t, label_t)
 
     def __len__(self):
-        return self.len 
+        return self.len
